ab14e77ee33c0d17d4800fcc5ee402b82c34124e
BashMobarmeg <baashmobarmeg@gmail.com>
2026-05-25 05:07:47 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,526 +1,119 @@
-@media (min-width: 500px) and (max-width: 599px) {
-    .div-footer {
-        grid-template-columns: repeat(2,1fr);
-        grid-template-rows: repeat(4, auto);
-    }
+// زر Home
+document.getElementById("Home").onclick = function () {
+    window.location.href = "#";
+};
 
-    .div-footer-btns {
-        grid-column: 1/2;
-        grid-row: 1/3;
-    }
+// زر شاهد اعمالي في الهيدر
+document.getElementById("view-work").onclick = function () {
+    document.querySelector(".hero-article4").scrollIntoView({
+        behavior: "smooth"
+    });
+};
 
-    h5 {
-        grid-column: 2/3;
-        grid-row: 1/2;
-        margin: 10px;
-        padding: 0px;
-        align-self: end;
-    }
+// زر تواصل معي
+document.getElementById("Contact").onclick = function () {
 
-    h6 {
-        grid-column: 2/3;
-        grid-row: 2/3;
-    }
+    const el = document.querySelector(".footer-btn");
 
+    let count = 0;
+
+    const interval = setInterval(() => {
+
+        el.classList.add("glow-active");
+
+        setTimeout(() => {
+            el.classList.remove("glow-active");
+        }, 200);
+
+        count++;
+
+        if (count === 2) {
+            clearInterval(interval);
+        }
+
+    }, 300);
+
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    });
+};
+
+// زر السيرة الذاتية
+document.getElementById("CV").onclick = function () {
+    window.open("", "_blank");
+};
+
+// زر شاهد اعمالي داخل الهيرو
+document.querySelector(".view-work-btn").onclick = function () {
+    document.querySelector(".hero-article4").scrollIntoView({
+        behavior: "smooth"
+    });
+};
+
+// زر visit1
+document.getElementById("visit1").onclick = function () {
+    window.open("https://bashmobarmeg.github.io/My-Portfolio/", "_blank");
+};
+
+// زر visit2
+document.getElementById("visit2").onclick = function () {
+    window.open("https://bashmobarmeg.github.io/Link-Hub/", "_blank");
+};
+
+// زر visit3
+document.getElementById("visit3").onclick = function () {
+    window.open("https://bashmobarmeg.github.io/calculator/", "_blank");
+};
+
+
+
+// زر GitHup
+document.getElementById("GitHup").onclick = function () {
+    window.open("https://github.com/bashmobarmeg", "_blank");
+};
+
+document.querySelector(".top-btn").onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
+
+document.getElementById("stop").onclick = function () {
+    const items = document.querySelectorAll(".div-article2, .div-article3, .div-article4");
+
+    items.forEach(el => {
+        el.classList.toggle("no-shadow");
+    });
+};
+
+let elements = document.querySelectorAll(
+    ".Project1, .Project2, .Project3, .visit"
+);
+
+let observer = new IntersectionObserver(function(entries){
     
+    entries.forEach(function(entry){
 
-}
-@media (min-width: 600px) and (max-width: 749px) {
-    .article2{
-        grid-column: 1/2;
-    }
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
 
-    .article3 {
-        grid-column: 2/3;
-    }
+    });
 
-    .content-div-article3 {
-        font-size: 25px;
-    }
+}, {
+    threshold: 0.2
+});
 
-    .header-btn {
-        font-size: 20px;
-    }
+elements.forEach(function(element){
+    observer.observe(element);
+});
 
-    .div-footer {
-        grid-template-columns: repeat(2, auto);
-        grid-template-rows: repeat(3, auto);
-    }
+document.getElementById("facebook").onclick = function () {
+        window.open("https://www.facebook.com/profile.php?id=61590326522454", "_blank");
+};
 
-    .div-footer-btns {
-        grid-column: 1/2;
-        grid-row: 2/4;
-    }
-
-    .footer-btn {
-        padding: 5px;
-    }
-
-    h5 {
-        grid-column: 2/3;
-        grid-row: 2/3;
-        margin: 10px;
-        padding: 0px;
-        align-self: end;
-    }
-
-    h6 {
-        grid-column: 2/3;
-        grid-row: 3/4;
-    }
-}
-
-@media (min-width: 750px) and (max-width: 949px) {
-    .article2{
-        grid-column: 1/2;
-    }
-
-    .article3 {
-        grid-column: 2/3;
-    }
-
-    .content-div-article3 {
-        font-size: 25px;
-    }
-
-    .Project1 {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: repeat(2,auto);
-        row-gap: 10px;
-    }
-
-    .desc-Project1 {
-        grid-column: 2/3;
-        grid-row: 2/3;
-    }
-
-    .img1 {
-        grid-column: 1/2;
-    }
-
-    #visit1 {
-        grid-column: 2/3;
-        grid-row: 2/3;
-        place-self: center;
-        
-    }
-
-    .Project3 {
-        grid-column: 1/2;
-    }
-
-    .Project2 {
-        grid-row: 2/3;
-        grid-column: 2/3;
-    }
-
-    .header-btn {
-        font-size: 20px;
-    }
-
-    .div-footer {
-        grid-template-columns: repeat(2, auto);
-        grid-template-rows: repeat(3, auto);
-    }
-
-    .div-footer-btns {
-        grid-column: 1/2;
-        grid-row: 2/4;
-    }
-
-    .footer-btn {
-        padding: 5px;
-    }
-
-    h5 {
-        grid-column: 2/3;
-        grid-row: 2/3;
-        margin: 10px;
-        padding: 0px;
-        align-self: end;
-    }
-
-    h6 {
-        grid-column: 2/3;
-        grid-row: 3/4;
-    }
-
-}
-
-@media  (min-width: 950px) and (max-width: 1219px){
-        .article2{
-        grid-column: 1/2;
-    }
-
-    .article3 {
-        grid-column: 2/3;
-    }
-
-    .content-div-article3 {
-        font-size: 25px;
-    }
-
-    .Project1 {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: repeat(2,auto);
-        row-gap: 10px;
-    }
-
-    .desc-Project1 {
-        grid-column: 2/3;
-        grid-row: 2/3;
-    }
-
-    .img1 {
-        grid-column: 1/2;
-    }
-
-    #visit1 {
-        grid-column: 2/3;
-        grid-row: 2/3;
-        place-self: center;
-        
-    }
-
-    .Project3 {
-        grid-column: 1/2;
-    }
-
-    .Project2 {
-        grid-row: 2/3;
-        grid-column: 2/3;
-    }
-
-    .header-btn {
-        font-size: 20px;
-    }
-
-    .div-footer {
-        grid-template-columns: repeat(3,auto);
-        grid-template-rows: auto 1fr auto 1fr;
-    }
-
-    .div-footer-btns {
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: repeat(2, 1fr);
-        grid-column: 1/2;
-        grid-row: 2/5;
-    }
-
-    #YouTube {
-        grid-column: 1/3;
-    }
-
-    h5 {
-        grid-column: 2/3;
-        grid-row: 2/3;
-        margin: 10px;
-        padding: 0px;
-        margin-right: auto;
-    }
-
-    h6 {
-        grid-column: 3/4;
-        grid-row: 2/3;
-        margin-left: auto;
-    }
-}
-
-@media (min-width: 1220px) and (max-width: 1299px) {
-    main {
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: auto 300px auto auto auto;
-    }
-
-    .article1 {
-        grid-column: 1/2;
-        padding-right: 50px;
-        padding-top: 20px;
-    }
-
-    .article2 {
-        grid-column: 2/3;
-        grid-row: 1/2;
-    }
-
-    .article3 {
-        grid-row: 1/2;
-        grid-column: 3/4;
-        width: 300px;
-        margin-left: auto;
-        margin-right: auto;
-    }
-
-    .article4 {
-        grid-column: 1/4;
-        width: 90%;
-        margin-left: auto;
-        margin-right: auto;
-    }
-
-    .Project1 {
-        grid-column: 1/2;
-    }
-
-    .Project2 {
-        grid-column: 2/3;
-    }
-
-    .Project3 {
-        grid-column: 3/4;
-    }
-    
-    .div-article4 {
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: repeat(1, auto);
-    }
-    .Edges-hero-article4 {
-        grid-column: 2/3;
-        top: 240px;
-    }
-
-    .header-btn {
-        font-size: 20px;
-    }
-
-    .div-footer {
-        grid-template-columns: repeat(3,auto);
-        grid-template-rows: auto 1fr auto 1fr;
-    }
-
-    .div-footer-btns {
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: repeat(2, 1fr);
-        grid-column: 1/2;
-        grid-row: 2/5;
-    }
-
-    #YouTube {
-        grid-column: 1/3;
-    }
-
-    h5 {
-        grid-column: 2/3;
-        grid-row: 2/3;
-        margin: 10px;
-        padding: 0px;
-        margin-right: auto;
-    }
-
-    h6 {
-        grid-column: 3/4;
-        grid-row: 2/3;
-        margin-left: auto;
-    }
-}
-
-@media (min-width: 1300px) and (max-width:1450px) {
-    main {
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: auto 300px auto auto auto;
-    }
-
-    .article1 {
-        grid-column: 1/2;
-        padding-right: 50px;
-        padding-top: 20px;
-    }
-
-    .article2 {
-        grid-column: 2/3;
-        grid-row: 1/2;
-    }
-
-    .title-div-article2 {
-        font-size: 35px;
-    }
-
-    .content-div-article2 {
-        font-size: 30px;
-    }
-
-    .article3 {
-        grid-row: 1/2;
-        grid-column: 3/4;
-        width: 300px;
-        margin-left: auto;
-        margin-right: auto;
-    }
-
-    .title-div-article3 {
-        font-size: 30px;
-    }
-
-    .article4 {
-        grid-column: 1/4;
-        width: 90%;
-        margin-left: auto;
-        margin-right: auto;
-    }
-
-    .Project1 {
-        grid-column: 1/2;
-    }
-
-    .Project2 {
-        grid-column: 2/3;
-    }
-
-    .Project3 {
-        grid-column: 3/4;
-    }
-    
-    .div-article4 {
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: repeat(1, auto);
-    }
-    .Edges-hero-article4 {
-        grid-column: 2/3;
-        top: 240px;
-    }
-
-    .header-btn {
-        font-size: 20px;
-    }
-
-    .div-footer {
-        grid-template-columns: repeat(3,auto);
-        grid-template-rows: repeat(2, auto);
-    }
-
-    .div-footer-btns {
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: repeat(1, 1fr);
-        grid-column: 1/2;
-        grid-row: 2/3;
-        border-radius: 30px;
-    }
-
-    .footer-btn {
-        padding: 10px;
-        border-radius: 50px;
-    }
-    
-    #YouTube {
-        grid-column: 2/4;
-    }
-
-    h5 {
-        grid-column: 2/3;
-        grid-row: 2/3;
-        margin: 10px;
-        padding: 0px;
-        margin-right: auto;
-    }
-
-    h6 {
-        grid-column: 3/4;
-        grid-row: 2/3;
-        margin-left: auto;
-    }
-}
-
-@media (min-width: 1451px) and (max-width:5000px) {
-        main {
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: auto 300px auto auto auto;
-    }
-
-    .article1 {
-        grid-column: 1/2;
-        padding-right: 50px;
-        padding-top: 20px;
-    }
-
-    .article2 {
-        grid-column: 2/3;
-        grid-row: 1/2;
-    }
-
-    .title-div-article2 {
-        font-size: 35px;
-    }
-
-    .content-div-article2 {
-        font-size: 30px;
-    }
-
-    .article3 {
-        grid-row: 1/2;
-        grid-column: 3/4;
-        width: 300px;
-        margin-left: auto;
-        margin-right: auto;
-    }
-
-    .title-div-article3 {
-        font-size: 30px;
-    }
-
-    .article4 {
-        grid-column: 1/4;
-        width: 90%;
-        margin-left: auto;
-        margin-right: auto;
-    }
-
-    .Project1 {
-        grid-column: 1/2;
-    }
-
-    .Project2 {
-        grid-column: 2/3;
-    }
-
-    .Project3 {
-        grid-column: 3/4;
-    }
-    
-    .div-article4 {
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: repeat(1, auto);
-    }
-    .Edges-hero-article4 {
-        grid-column: 2/3;
-        top: 240px;
-    }
-
-    .header-btn {
-        font-size: 20px;
-    }
-
-    .div-footer {
-        grid-template-columns: repeat(3,auto);
-        grid-template-rows: repeat(2, auto);
-    }
-
-    .div-footer-btns {
-        grid-template-columns: repeat(5, 1fr);
-        grid-template-rows: repeat(1, 1fr);
-        grid-column: 1/2;
-        grid-row: 2/3;
-        border-radius: 40px;
-    }
-
-    .footer-btn {
-        padding: 10px;
-        border-radius: 50px;
-    }
-    
-
-    h5 {
-        grid-column: 2/3;
-        grid-row: 2/3;
-        margin: 10px;
-        padding: 0px;
-        margin-right: auto;
-    }
-
-    h6 {
-        grid-column: 3/4;
-        grid-row: 2/3;
-        margin-left: auto;
-    }
-}
+document.getElementById("YouTube").addEventListener("click", function () {
+    window.open("https://youtube.com/@baashmobarmeg?si=4mctKGqCKmDlYWfw", "_blank");
+});
