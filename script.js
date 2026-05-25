f8e807f71b6dd724b1d87784e9bef6fd54a3de3a
BashMobarmeg <baashmobarmeg@gmail.com>
2026-05-25 07:17:27 +0400

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ document.getElementById("Contact").onclick = function () {
 
 document.getElementById("CV").addEventListener("click", function () {
   window.open(
-    "https://drive.google.com/file/d/1lMcVBlpDU738xgiZgvqDaUYY7T4934vL/preview",
+    "https://drive.google.com/file/d/1JtvB13aiiMZGpAMPs2pi9iN3v320vVHG/view?usp=drive_link",
     "_blank"
   );
 });
